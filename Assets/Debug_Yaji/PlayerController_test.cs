6f29ff61049df8718142f1ed8fe33cfba91103a0
surpass1116 <greenrikun16@gmail.com>
2026-06-30 11:24:20 +0900

    develop召喚用

--- Assets/Debug_Yaji/PlayerController_test.cs
+++ Assets/Debug_Yaji/PlayerController_test.cs
@@ -16,7 +16,7 @@ public class PlayerController_test : MonoBehaviour
     Vector3 moveVector;
     Vector3 attackPoint;
 
-    [SerializeField]float moveSpeed = 10;
+    [SerializeField] float moveSpeed = 10;
     [SerializeField] float moveRange = 15;
 
 
@@ -35,12 +35,35 @@ public class PlayerController_test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        Physics.SphereCast(transform.position, .5f, transform.forward, out hit, 1);
+        Debug.DrawRay(transform.position + transform.forward.normalized * 0.5f, transform.forward.normalized * 1);
+
         if (attackAction.triggered)
         {
-            attackPoint = transform.position;
-            attackRangePreview.transform.position = attackPoint;
-            attackRangePreview.SetActive (true);
+            if (hit.collider != null)
+            {
+                attackRangePreview.transform.position = new Vector3(hit.point.x, 0, hit.point.z);
+                attackRangePreview.transform.parent = hit.collider.transform;
+                attackRangePreview.SetActive(true);
+            }
+            if (attackRangePreview.activeSelf)
+            {
+                moveRange--;
+                attackRangePreview.transform.localScale = new Vector3(moveRange * 2, attackRangePreview.transform.localScale.y, moveRange * 2);
+            }
         }
+
+        Debug.Log(attackRangePreview.transform.localScale);
+        attackPoint = attackRangePreview.transform.position;
+
+        if (finalAttackAction.triggered)
+        {
+            attackRangePreview.SetActive(false);
+            moveRange = 15;
+            attackRangePreview.transform.localScale.Set(moveRange, attackRangePreview.transform.localScale.y, moveRange);
+        }
+
     }
 
     void FixedUpdate()
@@ -48,24 +71,35 @@ public class PlayerController_test : MonoBehaviour
         readVector = moveAction.ReadValue<Vector2>();
         moveVector = readVector.y * transform.forward + readVector.x * transform.right;
 
-        if(Vector3.Distance(attackPoint, transform.position) >= moveRange)
+        if (readVector.magnitude > 0.1)
         {
-            if(Vector3.Distance(attackPoint,transform.position) > Vector3.Distance(attackPoint, transform.position + moveVector))
+            if (attackRangePreview.activeSelf)
             {
-                p_Rigidbody.linearVelocity = moveVector * moveSpeed;
+                if (Vector3.Distance(attackPoint, transform.position) >= moveRange)
+                {
+                    if (Vector3.Distance(attackPoint, transform.position) > Vector3.Distance(attackPoint, transform.position + moveVector))
+                    {
+                        p_Rigidbody.linearVelocity = moveVector * moveSpeed;
 
+                    }
+                    else
+                    {
+                        p_Rigidbody.linearVelocity /= 1.2f;
+                    }
+                }
+                else
+                {
+                    p_Rigidbody.linearVelocity = moveVector * moveSpeed;
+                }
             }
             else
             {
-                p_Rigidbody.linearVelocity = Vector3.zero;
+                p_Rigidbody.linearVelocity = moveVector * moveSpeed;
             }
         }
         else
         {
-            p_Rigidbody.linearVelocity = moveVector * moveSpeed;
+            p_Rigidbody.linearVelocity /= 1.2f;
         }
-
-
-        //p_Rigidbody.linearVelocity = moveVector;
     }
 }
